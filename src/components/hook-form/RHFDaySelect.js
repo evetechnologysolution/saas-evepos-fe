b8634fcb20867b2760ae6ceb587d620d0fb3e4d4
evetechnologysolution <dev@evetechsolution.com>
2026-04-17 16:02:13 +0700

    update

--- src/components/hook-form/RHFDaySelect.js
+++ src/components/hook-form/RHFDaySelect.js
@@ -5,13 +5,20 @@ import { TextField, MenuItem } from '@mui/material';
 // Day options (0–6)
 // =====================
 const DAY_OPTIONS = [
-  { label: 'Sunday', value: 0 },
-  { label: 'Monday', value: 1 },
-  { label: 'Tuesday', value: 2 },
-  { label: 'Wednesday', value: 3 },
-  { label: 'Thursday', value: 4 },
-  { label: 'Friday', value: 5 },
-  { label: 'Saturday', value: 6 },
+  // { label: 'Sunday', value: 0 },
+  // { label: 'Monday', value: 1 },
+  // { label: 'Tuesday', value: 2 },
+  // { label: 'Wednesday', value: 3 },
+  // { label: 'Thursday', value: 4 },
+  // { label: 'Friday', value: 5 },
+  // { label: 'Saturday', value: 6 },
+  { label: 'Minggu', value: 0 },
+  { label: 'Senin', value: 1 },
+  { label: 'Selasa', value: 2 },
+  { label: 'Rabu', value: 3 },
+  { label: 'Kamis', value: 4 },
+  { label: 'Jumat', value: 5 },
+  { label: 'Sabtu', value: 6 },
 ];
 
 export default function RHFDaySelect({
@@ -43,7 +50,8 @@ export default function RHFDaySelect({
           {...other}
         >
           <MenuItem value="">
-            <em>All Days</em>
+            {/* <em>All Days</em> */}
+            <em>Setiap Hari</em>
           </MenuItem>
 
           {DAY_OPTIONS.map((option) => (
